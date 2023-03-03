b6146d9d39b430ce58b7fbe14b7bc341c6d8d69f
anamux <anaamux@gmail.com>
2023-03-03 17:39:39 -0300

    GPS Location Capture

--- app/src/main/java/com/anamuxfeldt/appcoordenadasgps/MainActivity.java
+++ app/src/main/java/com/anamuxfeldt/appcoordenadasgps/MainActivity.java
@@ -1,23 +1,25 @@
 package com.anamuxfeldt.appcoordenadasgps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.PackageManagerCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-public static final int REQUEST_LOCATION_PERMISSION = 1;
-
     String[] permissoesRequeridas = {Manifest.permission.ACCESS_FINE_LOCATION,
                                      Manifest.permission.ACCESS_COARSE_LOCATION};
 
@@ -48,8 +50,6 @@ public static final int REQUEST_LOCATION_PERMISSION = 1;
 
         }else {
 
-            latitude = 0.00;
-            longitude = 0.00;
 
         txtValorLatitude.setText(String.valueOf(latitude));
         txtValorLongitude.setText(String.valueOf(longitude));
@@ -67,14 +67,11 @@ public static final int REQUEST_LOCATION_PERMISSION = 1;
             capturarUltimaLocalizacaoValida();
         }
 
-
-
     }
 
     private boolean solicitarPermissaoParaObterLocalizacao() {
 
 
-        Toast.makeText(this, "App sem permissão de acesso ao GPS", Toast.LENGTH_LONG).show();
 
         List<String> permissoesNegadas = new ArrayList<>();
 
@@ -85,23 +82,55 @@ public static final int REQUEST_LOCATION_PERMISSION = 1;
             permissaoNegada = ContextCompat.checkSelfPermission(MainActivity.this, permissao);
 
             if(permissaoNegada != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "App sem permissão de acesso ao GPS", Toast.LENGTH_LONG).show();
                 permissoesNegadas.add(permissao);
-
-}
+            }
 
         }
+//Se permissoesNegadas não estiver vazio...
+        if (!permissoesNegadas.isEmpty()){
 
-        return true;
-    }
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    permissoesNegadas.toArray(new String[permissoesNegadas.size()]), APP_PERMISSOES_ID);
+
+            return false;
+        }else{
+            return true;
+        }
+       }
+
+
 
     private void capturarUltimaLocalizacaoValida() {
-        latitude = 1.98;
-        longitude = -1.67;
 
-        txtValorLatitude.setText(String.valueOf(latitude));
-        txtValorLongitude.setText(String.valueOf(longitude));
+        @SuppressLint("MissingPermission")
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+        if(location != null){
+
+            //Geopoint
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+        } else{
+
+            latitude = 0.00;
+            longitude = 0.00;}
+
+
+
+        txtValorLatitude.setText(formatarGeopoint(latitude));
+        txtValorLongitude.setText(formatarGeopoint(longitude));
 
         Toast.makeText(this, "Coordenadas obtidas com sucesso", Toast.LENGTH_LONG).show();
+
+    }
+    private String formatarGeopoint (double valor){
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.####");
+
+        return decimalFormat.format(valor);
 
     }
 }
