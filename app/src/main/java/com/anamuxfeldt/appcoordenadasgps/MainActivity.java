a982c03882af430cdedcaf69f8b4831391b06573
anamux <anaamux@gmail.com>
2023-03-02 21:11:09 -0300

    Init to GPS logic - PERMISSIONS

--- app/src/main/java/com/anamuxfeldt/appcoordenadasgps/MainActivity.java
+++ app/src/main/java/com/anamuxfeldt/appcoordenadasgps/MainActivity.java
@@ -1,15 +1,32 @@
 package com.anamuxfeldt.appcoordenadasgps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+public static final int REQUEST_LOCATION_PERMISSION = 1;
 
+    String[] permissoesRequeridas = {Manifest.permission.ACCESS_FINE_LOCATION,
+                                     Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    public static final int APP_PERMISSOES_ID = 2023;
     TextView txtValorLatitude, txtValorLongitude;
     double latitude, longitude;
-    boolean gpsAtivo = false;
+    ///Implementar a lógica de verificação da ativação da localização (LocationManager)
+    LocationManager locationManager;
+    boolean gpsAtivo = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
         txtValorLatitude = findViewById(R.id.txtValorLatitude);
         txtValorLongitude = findViewById(R.id.txtValorLongitude);
+// Conferir os serviços disponiveis via LocationManager
+
+        locationManager = (LocationManager) getApplication().getSystemService(Context.LOCATION_SERVICE);
+///isProviderEnable: checa se há algum provedor disponível, pode ser wifi, pacote de dados, GPS
+        gpsAtivo = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
 
         if(gpsAtivo){
 
@@ -31,31 +54,54 @@ public class MainActivity extends AppCompatActivity {
         txtValorLatitude.setText(String.valueOf(latitude));
         txtValorLongitude.setText(String.valueOf(longitude));
 
+            Toast.makeText(this, "Coordenadas não disponiveis", Toast.LENGTH_LONG).show();
+
         }
 
     }
 
     private void obterCoordenadas() {
-        boolean permissaoAtiva = false;
+        boolean permissaoAtiva = solicitarPermissaoParaObterLocalizacao();
 
         if(permissaoAtiva){
             capturarUltimaLocalizacaoValida();
-        }else{
-            solicitarPermissaoParaObterLocalizacao();
         }
 
 
+
     }
 
-    private void solicitarPermissaoParaObterLocalizacao() {
+    private boolean solicitarPermissaoParaObterLocalizacao() {
+
+
+        Toast.makeText(this, "App sem permissão de acesso ao GPS", Toast.LENGTH_LONG).show();
+
+        List<String> permissoesNegadas = new ArrayList<>();
+
+        int permissaoNegada;
+
+        for (String permissao : this.permissoesRequeridas) {
+
+            permissaoNegada = ContextCompat.checkSelfPermission(MainActivity.this, permissao);
+
+            if(permissaoNegada != PackageManager.PERMISSION_GRANTED){
+                permissoesNegadas.add(permissao);
+
+}
+
+        }
+
+        return true;
     }
 
     private void capturarUltimaLocalizacaoValida() {
-        latitude = 1.9876;
-        longitude = -1.6789;
+        latitude = 1.98;
+        longitude = -1.67;
 
         txtValorLatitude.setText(String.valueOf(latitude));
         txtValorLongitude.setText(String.valueOf(longitude));
+
+        Toast.makeText(this, "Coordenadas obtidas com sucesso", Toast.LENGTH_LONG).show();
 
     }
 }
